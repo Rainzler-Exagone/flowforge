1df01fb463dbd38c8d056264cdd9ce9bf21dad8c
Rainzler <yasser458891@gmail.com>
2026-08-17 00:40:28 +0100

    Add postgres service in Docker compose

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,9 +1,15 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
+
+export async function testDatabaseConnection() {
+  const result = await pool.query('SELECT NOW()');
+
+  console.log('Database connected:', result.rows[0]);
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
+import { testDatabaseConnection } from './db';
 
 async function bootstrap() {
+    await testDatabaseConnection();
+  
   const app = await NestFactory.create(ApiModule);
   await app.listen(process.env.port ?? 3000);
 }
